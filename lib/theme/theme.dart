f80bc06b40ab514fe2906b884697e07d0200fdf2
orion <nnmm5122004@gmail.com>
2025-07-11 18:58:11 +0630

    added required dependencies

---
+++ lib/theme/theme.dart
@@ -0,0 +1,17 @@
+import 'dart:ui';
+
+class ThemeService{
+
+ //Color
+ static final Color PRIMARY_BLACK_COLOR = Color(0xFF292C2D);
+ static final Color SECONDARY_BLACK_COLOR = Color(0xFF111315);
+ static final Color WHITE_COLOR = Color(0xFFF1F7F7);
+ static final Color LIGHT_GREEN_COLOR = Color(0xFF74FFC5);
+ static final Color GREEN_COLOR = Color(0xFF019D90);
+
+
+
+ //Font Size
+static final double HEADING_FONT_SIZE = 22;
+
+}
